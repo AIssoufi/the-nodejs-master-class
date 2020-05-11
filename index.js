f1f2af663c927a6b95b154c8d04a9ae703ec47b8
Goku <issoufi.adam1@gmail.com>
2020-05-11 23:12:13 +0200

    Starting a server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello the world!");
+// dependencies
+const http = require('http');
+
+// The server should respond to all request with a string
+const server = http.createServer((req, res) => {
+    res.end('Hello !');
+});
+
+// Start the server, and have it to listen on port 3000
+const port = 300;
+server.listen(port, () => {
+    console.log(`Server start on http://localhost:${port}`);
+});
